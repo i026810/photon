559a3ea73f8908fb3341cf5c6f91fe94aac0eed7
i026810 <70507234+i026810@users.noreply.github.com>
2021-10-14 12:06:00 +0900

    Add files via upload

---
+++ cloud-app-info.js
@@ -0,0 +1,8 @@
+﻿var AppInfo = {
+    //	Wss: true,
+    AppId: "2b943fbd-1c72-4f73-a074-7628c0494553",
+    AppVersion: "1.0",
+    //	MasterServer: "localhost:9090"
+    //  NameServer: "ws://localhost:9093"
+    //  FbAppId: "you fb app id", 
+}
